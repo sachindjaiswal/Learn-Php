ca5da60f3afa1dfef8f591bab736544c1cc9b3a2
sachindjaiswal <sachin.dharmendrajaiswal@gmail.com>
2026-06-24 03:19:20 +0530

    aaj ka pura din toh setup karne mien hii chala gaya

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learn PHP</title>
+</head>
+<body>
+    <div class="container">
+        this is the day 1 of learning php 
+        <?php
+            echo "this is printed using php"
+        ?>
+    </div>
+</body>
+</html>
