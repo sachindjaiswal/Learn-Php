a8bc934de3b4762fa86dfb0949670327a857dd86
sachindjaiswal <sachin.dharmendrajaiswal@gmail.com>
2026-06-24 17:20:30 +0530

    learned basic variables and echo statements

--- index.php
+++ index.php
@@ -9,8 +9,30 @@
     <div class="container">
         this is the day 1 of learning php 
         <?php
-            echo "this is printed using php"
+            echo "this is printed using php";
         ?>
     </div>
+
+    <?php
+        echo "This is basically the print statement";
+    ?>
+    </br>
+    <?php
+        
+        echo " i love shwarma";
+        
+        echo "The best thing about php is that you can inclide php within an html file "
+    ?>
+    <br>
+    <button>Want Shwarma</button>
+    <br>
+    <?php
+    
+         
+        $name = "Sachin";
+        $age = 20;
+        echo "Hello {$name} <br>";
+        echo "The age of {$name} is {$age}";
+    ?>
 </body>
 </html>
