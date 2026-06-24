0cefa3fda0b4ec28d70fc294696c93aa30484b10
sachindjaiswal <sachin.dharmendrajaiswal@gmail.com>
2026-06-24 18:22:13 +0530

    basic forms done

---
+++ forms.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="forms.php" method="post">
+    <label>username:</label><br>
+    <input type="text" name="username"><br>
+    <label>password:</label><br>
+    <input type="password" name="password"><br>
+    <input type="submit" value="log in">
+    </form>
+    
+</body>
+</html>
+<?php
+    echo $_POST["username"];
+    echo $_POST["password"]
+    ?>
