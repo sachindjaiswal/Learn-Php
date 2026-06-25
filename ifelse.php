fd5ec339bd46b382cb0117c9c5284c96ba640bc7
sachindjaiswal <sachin.dharmendrajaiswal@gmail.com>
2026-06-25 18:20:54 +0530

    Done with loops

---
+++ ifelse.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>if else sikhenge</title>
+</head>
+<body>
+    
+</body>
+</html>
+<?php
+    $age = 18;
+    if($age >= 18 ){
+        echo "You can enter the site";
+    
+    }elseif($age > 40){
+        echo "Your too old , so please watch devotional content";
+    }
+    else{
+        echo "please fuck off";
+    };
+?>
---
+++ loop.php
@@ -0,0 +1,12 @@
+<?php
+    $name = "Sachin";
+    for($i=0;$i<5;$i++){
+        echo "Hello {$name} {$i} . <br>";
+    }
+    $a = 0 ;
+
+    while ($a <= 10) {
+        $a++;
+        echo $a ."<br>" ;    
+    }
+?>
