ed93e0a50c458be58bb4ccc4d10a88a356469ef6
sachindjaiswal <sachin.dharmendrajaiswal@gmail.com>
2026-06-25 19:24:36 +0530

    implemented the array in hotel project

---
+++ associativeArray.php
@@ -0,0 +1,12 @@
+<?php
+    $menus = array("biryani"=>100 ,"Soup"=>50 , "Shwarma"=>90 , "Noodle"=>100 , "Triple Rice"=>150);
+    $menus["Rice"] = 90 ; 
+    $menus["biryani"] = 150;
+
+    echo $menus["biryani"];
+    foreach($menus as $dish=>$price ){
+         echo " {$dish} = {$price} <br> ";
+    }
+
+
+?>
--- hotel/hotelForms.php
+++ hotel/hotelForms.php
@@ -1,13 +1,15 @@
 <?php
-    $menu = "biryani";
-    $price = 106;
+    $menus = array("biryani"=>100 ,"Soup"=>50 , "Shwarma"=>90 , "Noodle"=>100 , "Triple Rice"=>150);
+    
 
     $order = $_GET["dishname"];
-    $quantity = $_GET["dishquant"];
-    $total = $price * $quantity;
-    if ($menu === $order) {
-        echo "you have order {$order} and the price pf its one plate is {$price} so your total is {$total}  ";
-    }else{
+    if ($menus[$order]) {
+        $quantity = $_GET["dishquant"];
+        $total = $menus[$order] * $quantity;
+        echo "you have order {$order} and the price pf its one plate is {$menus[$order]} so your total is {$total}  ";
+        
+    }
+    else{
         echo "we dont serve {$order}";
     }
 ?>
