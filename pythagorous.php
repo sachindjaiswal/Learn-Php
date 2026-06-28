217f2fdc5e426f2674e0e7e1f15645112c4ddbfc
sachindjaiswal <sachin.dharmendrajaiswal@gmail.com>
2026-06-28 20:59:31 +0530

    done with functions

---
+++ pythagorous.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Triangle</title>
+</head>     
+<body>
+    <form action="pythagorous.php" method="post">
+        <label >SideA</label>
+        <input type="number" name="sideA"><br>
+        <label >SideB</label>
+        <input type="number" name="sideB"><br>
+        <input type="submit" value="submit" name="submit">
+    </form>
+</body>
+</html>
+
+<?php 
+
+    $sideA= $_POST["sideA"];
+    $sideB = $_POST["sideB"];
+
+    function hypo(int $sideA ,int  $sideB){
+        $hypo = sqrt(($sideA**2 + $sideB**2));
+        return  $hypo;
+    }
+    if($_POST["submit"]){
+
+    
+    }
+?>
