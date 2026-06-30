76c07973d5633fe4eb468eab4e7198a9d3495912
sachindjaiswal <sachin.dharmendrajaiswal@gmail.com>
2026-06-30 21:03:34 +0530

    done with validation

--- validation.php
+++ validation.php
@@ -6,26 +6,27 @@
     <title>Sanitization</title>
 </head>
 <body>
-    <form action="sanitization.php" method="post">
+    <form action="validation.php" method="post">
         <label>username</label>
         <input type="text" name="username">
         <label>Age</label>
-        <input type="number" name="age">
+        <input type="text" name="age">
         <label>EMAIL</label>
         <input type="text" name="email">
         <input type="submit" value="submit" name="submit">
     </form>
 </body>
 </html>
+
 <?php
 
     if(isset($_POST["submit"])){
-        // $username = $_POST["username"];
-        $username = filter_input(INPUT_POST , "username" ,FILTER_SANITIZE_SPECIAL_CHARS);
-        $age = filter_input(INPUT_POST , "age" , FILTER_SANITIZE_NUMBER_INT); 
-        $email = filter_input(INPUT_POST , "email" , FILTER_SANITIZE_EMAIL);
-        echo "Hello {$username} you are {$age} years old and you mail is {$email}";
-        
+        $age = filter_input(INPUT_POST , "age" , FILTER_VALIDATE_INT);
+        if(empty($age)){
+            echo "That age is not valid , please enter a valid number";
+        }else{
+            echo "your age is {$age}";
+        }
     }
 
 ?>
