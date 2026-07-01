8e7593a68fd78db871e3f17cefb347d3e8e1e66d
sachindjaiswal <sachin.dharmendrajaiswal@gmail.com>
2026-07-01 18:12:53 +0530

    Done with include function of php

---
+++ website/about.php
@@ -0,0 +1,20 @@
+
+
+<?php
+    include("header.html")
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h3>THis is the about page </h3>
+</body>
+</html>
+
+<?php
+    include("footer.html")
+?>
