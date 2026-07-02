f4630be59e3f1f5532a31e0bce7ae00f005b43d7
sachindjaiswal <sachin.dharmendrajaiswal@gmail.com>
2026-07-02 19:10:42 +0530

    done with server super global variable

---
+++ server.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="server.php" method="post">
+    <label>username:</label><br>
+    <input type="text" name="username"><br>
+    <label>password:</label><br>
+    <input type="password" name="password"><br>
+    <input type="submit" value="log in">
+    </form>
+    
+</body>
+</html>
+<?php
+
+    foreach($_SERVER as $key=>$value){
+        echo "{$key} = {$value}<br>";
+    }
+
+?>
